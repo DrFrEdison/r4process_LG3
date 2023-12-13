ad9aca324941dd5223ba48210c4cf034113e318a
Markus <kurtz@mailbox.org>
2023-12-13 08:55:41 +0100

    Update LG3_spectral_analysis.R

--- function/LG3_spectral_analysis.R
+++ function/LG3_spectral_analysis.R
@@ -14,7 +14,7 @@ if (length(args) >= 1) {
   # Split the batch filename by underscores
   filename_parts <- unlist(strsplit(batch_filename, "_"))
   
-  if (length(filename_parts) >= 3) {
+  if (length(filename_parts) == 3) {
     # Extract the type and n values
     batch_type <- filename_parts[2]
     batch_n <- as.numeric(filename_parts[3])
@@ -85,20 +85,20 @@ if (length(args) >= 1) {
              , y3
              , xpd = T)
     rect(x1,y1,x2,y2, xpd = T)
-
-timestamps <- c(as.character(dat$raw$datetime), as.character(as.POSIXct(lg3$sql$time.t1, tz = "UTC")))
-timestamps <- as.POSIXct(timestamps, tz   = "UTC")
-
-timestamps.list <- list()
-for(i in 2 : length( timestamps )){
-  
-  timestamps.list[[ i - 1]] <- as.numeric(difftime(timestamps[ i ], timestamps[ i - 1], units = "sec"))
-  
-}
-
-timestamps.list <- unlist( timestamps.list )    
-
-
+    
+    timestamps <- c(as.character(dat$raw$datetime), as.character(as.POSIXct(lg3$sql$time.t1, tz = "UTC")))
+    timestamps <- as.POSIXct(timestamps, tz   = "UTC")
+    
+    timestamps.list <- list()
+    for(i in 2 : length( timestamps )){
+      
+      timestamps.list[[ i - 1]] <- as.numeric(difftime(timestamps[ i ], timestamps[ i - 1], units = "sec"))
+      
+    }
+    
+    timestamps.list <- unlist( timestamps.list )    
+    
+    
     cumsump <- cumsum(c(0, timestamps.list))
     if( max(cumsump) / 60 < 180*60 )
       text(x3 + diff(par("usr")[c(1,2)]) * .0125/2, y3
@@ -112,6 +112,75 @@ timestamps.list <- unlist( timestamps.list )
     
     dev.off()
   }
+  
+  if (length(filename_parts) > 3) {
+    # Extract the type and n values
+    batch_type <- filename_parts[2]
+    batch_n <- as.numeric(filename_parts[3])
+    batch_lambda <- as.numeric(filename_parts[4])
+    
+    # Use batch_type and batch_n in your R code
+    cat("Batch Type:", batch_type, "\n")
+    cat("Batch n:", batch_n, "\n")
+    cat("Batch lambda:", batch_lambda, "\n")
+    
+    # Zeitspanne für Abfrage ####
+    lg3$sql$time.t0 <- substr(as.character(Sys.time() - batch_n * 60), 1, 19)
+    lg3$sql$time.t1 <- substr(as.character(Sys.time()), 1, 19)
+    
+    lg3$query <- sql_query(driver = lg3$para$sql_driver
+                           , server = lg3$para$sql_server
+                           , database = lg3$para$database
+                           , t0 = lg3$sql$time.t0
+                           , t1 = lg3$sql$time.t1
+                           , ref.sql = paste0(lg3$wd, "/sql/dt_sql_bgd_XGuard.sql")
+                           , drk.sql = paste0(lg3$wd, "/sql/dt_sql_drk_XGuard.sql")
+                           , spc.sql = spc.sql
+                           , wl = 190 : 598
+                           , ask = c(batch_type)
+                           , wd = lg3$wd 
+                           , export_path = paste0("C://Users/", lg3$para$uid, "/Documents/")
+                           , bat = T)
+    
+    # Read .csv file
+    dat <- list()
+    dat$raw <- lg3$query[[ 2 ]]
+    dat$ppp <- transfer_csv.num.col(dat$raw)
+    
+    setwd(save.wd)
+    setwd("..")
+    
+    filenamep <- paste(gsub(":", "", gsub(" ", "_", gsub("-", "", paste( substr(lg3$sql$time.t0, 3, nchar( lg3$sql$time.t0 ))
+                                                                         , substr(lg3$sql$time.t1, 3, nchar( lg3$sql$time.t0 ))
+                                                                         , sep = "_bis_"))))
+                       , paste0("lambda", batch_lambda)
+                       , gsub("\\.csv$", "\\.png", substr(basename(lg3$query[[ 1 ]]), 23, nchar( basename(lg3$query[[ 1 ]]) )))
+                       , sep = "_")
+    filenamep <- gsub("_R_export", "", filenamep)
+    
+    png(filenamep
+        ,xxx<-4800,xxx/16*9,"px",12,"white",res=500,"sans",T,"cairo")
+    
+    ydat1 <- dat$raw[ , get( grep(batch_lambda, names( dat$raw), value = T))]
+    
+    plot(as.POSIXct(dat$raw$datetime, tz = "UTC")
+         , ydat1
+         , xlab = ""
+         , ylab = ""
+         , col = y1.col <- hcl.colors( length(ydat1)))
+    
+    mtext(ifelse(batch_type == "spc"
+                 , paste0("Absorption bei ", batch_lambda, "nm zwischen\n"
+                          , lg3$sql$time.t0, " und ", lg3$sql$time.t1
+                          , " in ", lg3$para$location, ", Linie ", lg3$para$line)
+                 , paste0("Counts bei ", batch_lambda, "nm zwischen "
+                          , lg3$sql$time.t0, " und ", lg3$sql$time.t1
+                          , "\nin ", lg3$para$location, ", Linie ", lg3$para$line))
+          , 2, 2)
+    
+    dev.off()
+  }
+  
 } else {
   cat("Invalid filename format. Please use 'plot_type_n.bat' format.\n")
 }
